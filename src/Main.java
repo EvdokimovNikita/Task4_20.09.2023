f546305fdb1e3fe3cb5416952ff9a32b03c669b4
Nikita Evdokimov <70598794+EvdokimovNikita@users.noreply.github.com>
2023-09-20 23:02:57 +0300

    second correction

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 // Бинарный поиск по массиву с рекурсией без использования Arrays.binarySearch
 public class Main {
     public static void main(String[] args) {
-        int[] testArrays = {2, 3, 6, 7, 10, 11, 12, 15, 19, 21, 22, 25, 29, 32,33};
+        int[] testArrays = {2, 3, 6, 7, 10, 11, 12, 15, 19, 21, 22, 25, 29, 32, 33, 42};
         System.out.println("Исходный массив: " + Arrays.toString(testArrays));
         //int searchNum = 4;
         //System.out.println("Искомый элемент: " + searchNum);
@@ -22,18 +22,16 @@ public class Main {
 
     public static int binarySearchWithRecursion(int startPoint, int endPoint, int searchNum, int[] resultsNewArrays) {
         int result = resultsNewArrays[(startPoint + endPoint) / 2];
-        if (startPoint+1 == endPoint) {
+        if (startPoint + 1 == endPoint) {
             return -1;
         }
         if (result == searchNum) {
             return (startPoint + endPoint) / 2;
         } else if (result > searchNum) {
             endPoint = (startPoint + endPoint) / 2;
-            result = resultsNewArrays[(startPoint + endPoint) / 2];
             return binarySearchWithRecursion(startPoint, endPoint, searchNum, resultsNewArrays);
         } else if (result < searchNum) {
             startPoint = (startPoint + endPoint) / 2;
-            result = resultsNewArrays[(startPoint + endPoint) / 2];
             return binarySearchWithRecursion(startPoint, endPoint, searchNum, resultsNewArrays);
         }
         return result;
